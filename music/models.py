bcb3b67aef8293c1a46f1e54f26438c72c906df1
Evgeniy <evgeniy.web@ukr.net>
2017-11-19 23:05:24 +0100

    form added

--- music/models.py
+++ music/models.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
 
 class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
-    genre = models.CharField(max_length=50)
+    image = models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'id': self.id})
 
     def __str__(self):
-       return self.album_title + ' - ' + self.artist
+        return self.album_title + ' - ' + self.artist
 
 
 class Song(models.Model):
@@ -21,4 +23,4 @@ class Song(models.Model):
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
-       return self.song_title
+        return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,29 +1,48 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Album, Song
+from django.views import generic
 
 
-def index(request):
-    albums = Album.objects.all()
-    context = {'albums': albums}
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, id=album_id)
-    return render(request, 'music/detail.html', {'album': album})
+class DetailView(generic.DetailView):
+    template_name = 'music/detail.html'
+    model = Album
 
 
-def favorite(request, album_id):
-    album = get_object_or_404(Album, id=album_id)
-    try:
-        song = album.song_set.get(id=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': 'No way.'
-        })
-    else:
-        song.is_favorite = not song.is_favorite
-        song.save()
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'image']
+
+# def index(request):
+#     albums = Album.objects.all()
+#     context = {'albums': albums}
+#     return render(request, 'music/index.html', context)
+#
+#
+# def detail(request, album_id):
+#     album = get_object_or_404(Album, id=album_id)
+#     return render(request, 'music/detail.html', {'album': album})
+#
+#
+# def favorite(request, album_id):
+#     album = get_object_or_404(Album, id=album_id)
+#     try:
+#         song = album.song_set.get(id=request.POST['song'])
+#     except (KeyError, Song.DoesNotExist):
+#         return render(request, 'music/detail.html', {
+#             'album': album,
+#             'error_message': 'No way.'
+#         })
+#     else:
+#         song.is_favorite = not song.is_favorite
+#         song.save()
+#         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
